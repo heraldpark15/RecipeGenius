2d0b90162f21c617d9c6e4fda2071e76c828cfe1
Herald Park <herald1999@gmail.com>
2025-03-08 14:24:31 -0500

    Add config

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
-BASE_URL = os.getenv("BASE_URL")
-HUGGING_TOKEN = os.getenv("HUGGING_TOKEN")
+API_KEY = "ENTER YOUR KEY HERE"
+BASE_URL = "https://openrouter.ai/api/v1"
+HUGGING_TOKEN = "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"
