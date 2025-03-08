54486477510b1fc0c23be0a58ecf843fbf51a359
Herald Park <herald1999@gmail.com>
2025-03-08 12:16:45 -0500

    Update main

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from ui.chat_interface import ChatInterface
 from services.openai_service import OpenAIService
 from services.image_service import ImageService
 from utils.state_manager import StateManager
+from ui.design import apply_styles
 
-st.title("RecipeGenius 🧞 - From Ingredients to Plate 🍽️")
+apply_styles()
+
+#st.title("RecipeGenius 🧞 - From Ingredients to Plate 🍽️")
+st.markdown('<h1 class="slide-text">RecipeGenius 🧞 - From Ingredients to Plate 🍽️</h1>', unsafe_allow_html=True)
 
 # Initialize state and services
 state_manager = StateManager()
